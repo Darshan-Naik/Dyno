bbc39f781c80dcbb52e1c7a21933d1d3762c8900
Darshan Naik <darshan.naik@wrkspot.com>
2025-05-12 18:52:17 +0530

    new quick note added"

--- src/component/Apps/QuickNote/index.tsx
+++ src/component/Apps/QuickNote/index.tsx
@@ -1,60 +1,61 @@
-import {
-  Editor,
-  Toolbar,
-  EditorProvider,
-  BtnBold,
-  BtnItalic,
-  BtnUnderline,
-  BtnStrikeThrough,
-  BtnBulletList,
-  BtnUndo,
-  BtnRedo,
-  Separator,
-  BtnNumberedList,
-  BtnClearFormatting,
-} from "react-simple-wysiwyg";
 import { updateQuickNote } from "./store/quickNote.action";
 import { observer } from "mobx-react-lite";
 import { quickNoteStore } from "./store/quickNote.store";
+import {
+  BoldItalicUnderlineToggles,
+  diffSourcePlugin,
+  headingsPlugin,
+  linkPlugin,
+  listsPlugin,
+  ListsToggle,
+  markdownShortcutPlugin,
+  MDXEditor,
+  quotePlugin,
+  tablePlugin,
+  toolbarPlugin,
+  UndoRedo,
+} from "@mdxeditor/editor";
+import "./style.css";
+import { useRef } from "react";
 
 const QuickNote = () => {
-  function onChange(e) {
-    updateQuickNote(e.target.value);
+  const ref = useRef(null);
+  function onChange(value) {
+    updateQuickNote(value);
   }
   return (
-    <div className="flex-1 p-2 flex flex-col">
-      <EditorProvider>
-        <Toolbar className="bg-transparent flex gap-4 items-center justify-between border-b border-gray-500 px-4 py-2 mb-2 ">
-          <div className="flex gap-4 items-center">
-            <BtnUndo className="text-secondary font-normal bg-transparent hover:text-primary" />
-            <BtnRedo className="text-secondary font-normal bg-transparent hover:text-primary" />
-            <Separator />
-          </div>
-          <div className="flex gap-4 items-center">
-            <BtnBold className="text-secondary font-normal bg-transparent data-[active=true]:text-primary hover:text-primary" />
-            <BtnItalic className="text-secondary font-normal bg-transparent data-[active=true]:text-primary hover:text-primary" />
-            <BtnUnderline className="text-secondary font-normal bg-transparent data-[active=true]:text-primary hover:text-primary" />
-            <BtnStrikeThrough className="text-secondary font-normal bg-transparent data-[active=true]:text-primary hover:text-primary" />
-            <BtnBulletList className="text-secondary font-normal bg-transparent data-[active=true]:text-primary hover:text-primary" />
-            <BtnNumberedList className="text-secondary font-normal bg-transparent data-[active=true]:text-primary hover:text-primary" />
-          </div>
-          <div className="flex gap-4 items-center">
-            <Separator />
-            <BtnClearFormatting className="text-secondary font-normal bg-transparent hover:text-primary" />
-          </div>
-        </Toolbar>
-        <Editor
-          value={quickNoteStore.note}
-          autoFocus
-          onChange={onChange}
-          placeholder="No note here yet! Time to add some magic ✨"
-          containerProps={{
-            className:
-              "flex-1 !border-none !overflow-auto max-w-full text-primary",
-          }}
-          className="placeholder:italic placeholder:font-extralight placeholder:text-xs"
-        />
-      </EditorProvider>
+    <div
+      className="flex-1 p-2 flex flex-col overflow-auto"
+      onClick={() => ref.current?.focus()}
+    >
+      <MDXEditor
+        ref={ref}
+        markdown={quickNoteStore.note}
+        className="dark-theme dark-editor "
+        contentEditableClassName="prose dark:prose-invert prose-headings:mt-0 prose-headings:mb-0 prose-p:mt-0 prose-p:mb-0"
+        plugins={[
+          headingsPlugin(),
+          listsPlugin(),
+          linkPlugin(),
+          quotePlugin(),
+          markdownShortcutPlugin(),
+          tablePlugin(),
+          diffSourcePlugin({
+            viewMode: "rich-text",
+          }),
+          toolbarPlugin({
+            toolbarClassName: "my-classname",
+            toolbarContents: () => (
+              <div className="flex justify-between items-center w-full">
+                <UndoRedo />
+                <BoldItalicUnderlineToggles />
+                <ListsToggle />
+              </div>
+            ),
+          }),
+        ]}
+        onChange={onChange}
+      />
     </div>
   );
 };
