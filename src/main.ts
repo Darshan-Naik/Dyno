2440748294f3c0d18c61bf721c98e5e67517236f
Darshan Naik <darshan.naik@wrkspot.com>
2025-05-24 01:29:46 +0530

    updated app load from remote

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ if (started) {
   app.quit();
 }
 
+const APP_URL = "https://dyno-rho.vercel.app";
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -20,15 +22,17 @@ const createWindow = () => {
     },
     darkTheme: true,
     icon: "./assets/icons/icon.png",
+    backgroundColor: "#181818",
   });
 
-  // and load the index.html of the app.
+  // Load the app
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
-    mainWindow.loadFile(
-      path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
-    );
+    mainWindow.loadURL(APP_URL);
+    // mainWindow.loadFile(
+    //   path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
+    // );
   }
 
   // Open the DevTools.
@@ -38,7 +42,7 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on("ready", createWindow);
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -66,8 +70,10 @@ app.setAboutPanelOptions({
 ipcMain.handle("get-clipboard-text", () => {
   return clipboard.readText();
 });
+
 ipcMain.handle("set-clipboard-text", (_, text: string) => {
   return clipboard.writeText(text);
 });
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
