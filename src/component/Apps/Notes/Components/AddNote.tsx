0c0582406af4eafc41bbc1a52cb67a83c12ba3b3
Darshan Naik <darshan.naik@wrkspot.com>
2025-06-26 20:59:16 +0530

    ui update

--- src/component/Apps/Notes/Components/AddNote.tsx
+++ src/component/Apps/Notes/Components/AddNote.tsx
@@ -11,7 +11,7 @@ const AddNote = ({ className, showLabel = true, onClick }: AddNoteProps) => {
   return (
     <button
       className={twMerge(
-        "cursor-pointer text-muted-foreground hover:text-foreground text-xs flex gap-2 items-center bg-background rounded-sm py-1 px-2",
+        "cursor-pointer text-muted-foreground hover:text-foreground text-sm flex gap-2 items-center bg-background rounded-sm py-1 px-2",
         className
       )}
       onClick={onClick}
--- src/component/Apps/Tasks/Components/AddTask.tsx
+++ src/component/Apps/Tasks/Components/AddTask.tsx
@@ -12,7 +12,7 @@ const AddTask = ({ className, showLabel = true }: AddTaskProps) => {
   return (
     <button
       className={twMerge(
-        "text-muted-foreground transition-colors hover:text-foreground text-xs flex gap-2 items-center bg-background rounded-sm py-1 px-2 cursor-pointer",
+        "text-muted-foreground transition-colors hover:text-foreground text-sm flex gap-2 items-center bg-background rounded-sm py-1 px-2 cursor-pointer",
         className
       )}
       onClick={() => useViewTransition(addNewTask)}
